cbaf4ad4ad6445affe899bf84b08060a9ade5e4e
Kyle Chung <kylechun9@gmail.com>
2016-01-09 15:58:30 +0800

    add comments

--- windows_encoding_examples/convert2utf8.R
+++ windows_encoding_examples/convert2utf8.R
@@ -1,4 +1,11 @@
+#!/usr/bin/env Rscript
 
+# when R in Windows write a file in text mode but the UTF-8 encoding can not be resolved,
+# escaped code point literals are used to represent the original text
+# (see example.dat.csv for example)
+# this script provides a viable way to force the escaped format back to corrected UTF
+# however the best practice in dealing with UTF-8 in Windows is not to write file in text mode but in binary mode
+# or even better, don't use Windows :)
 
 library(Unicode)
 library(data.table)
