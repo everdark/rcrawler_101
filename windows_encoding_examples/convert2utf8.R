9f5744c44a33e7ed1e28a0b18e42dc02ed18d1e5
Kyle Chung <kylechun9@gmail.com>
2016-01-09 16:06:06 +0800

    correct grama

--- windows_encoding_examples/convert2utf8.R
+++ windows_encoding_examples/convert2utf8.R
@@ -1,11 +1,12 @@
 #!/usr/bin/env Rscript
 
-# when R in Windows write a file in text mode but the UTF-8 encoding can not be resolved,
-# escaped code point literals are used to represent the original text
+# when R in Windows writes a file in text mode but the UTF-8 encoding can not be resolved,
+# escaped code-point literals are used to represent the original text
 # (see example.dat.csv for example)
-# this script provides a viable way to force the escaped format back to corrected UTF
-# however the best practice in dealing with UTF-8 in Windows is not to write file in text mode but in binary mode
-# or even better, don't use Windows :)
+# this script provides a viable way to force the escaped format back to corrected UTF-8 strings
+# however the best practice in dealing with UTF-8 in Windows is:
+# not to write file in text mode but in binary mode in the first place
+# or even better, just don't use Windows :)
 
 library(Unicode)
 library(data.table)
